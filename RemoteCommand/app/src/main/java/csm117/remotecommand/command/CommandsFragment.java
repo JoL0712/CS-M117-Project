4b5811cd3e3721721240f214e49aef61b366785a
John Lee <johnlee07@ucla.edu>
2015-11-05 23:13:41 -0800

    can edit existing commands by holding command

--- RemoteCommand/app/src/main/java/csm117/remotecommand/command/CommandsFragment.java
+++ RemoteCommand/app/src/main/java/csm117/remotecommand/command/CommandsFragment.java
@@ -1,49 +1,31 @@
 package csm117.remotecommand.command;
 
-import android.app.ProgressDialog;
-import android.content.ContentProviderOperation;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.OperationApplicationException;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Gravity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Scroller;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 
 import csm117.remotecommand.R;
 import csm117.remotecommand.network.Connection;
 
-public class CommandsFragment extends Fragment implements AdapterView.OnItemClickListener {
+public class CommandsFragment extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
     private ListView mListView = null;
-    private List<CommandItem> mCommands;
-    private CommandListViewAdapter mAdapter = null;
+    private static List<CommandItem> mCommands;
+    private static CommandListViewAdapter mAdapter = null;
 
     @Nullable
     @Override
@@ -57,6 +39,7 @@ public class CommandsFragment extends Fragment implements AdapterView.OnItemClic
         mAdapter = new CommandListViewAdapter(getActivity(), R.layout.command_list_item, mCommands);
         mListView.setAdapter(mAdapter);
         mListView.setOnItemClickListener(this);
+        mListView.setOnItemLongClickListener(this);
 
         return view;
     }
@@ -85,5 +68,49 @@ public class CommandsFragment extends Fragment implements AdapterView.OnItemClic
             //check command version (version changes if it is updated)
         //else send the actual command
         Connection.getInstance().sendCommand((CommandItem) mListView.getItemAtPosition(position));
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        CommandItem ci = (CommandItem) mListView.getItemAtPosition(position);
+
+        Intent intent = new Intent();
+        intent.putExtra("CommandIndex", position);
+
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        EditorFragment cb = new EditorFragment();
+        cb.setArguments(intent.getExtras());
+        ft.add(R.id.main_layout, cb);
+        ft.addToBackStack("CommandEditorLayout");
+        ft.commit();
+        return true;
+    }
+
+    public static class EditorFragment extends Fragment {
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.command_editor, container, false);
+            Bundle args = getArguments();
+            final CommandItem ci = mCommands.get(args.getInt("CommandIndex"));
+            final EditText name = ((EditText) view.findViewById(R.id.command_editor_name)), script = ((EditText) view.findViewById(R.id.command_editor_script));
+            name.setText(ci.getCommandName());
+            script.setText(ci.getCommand());
+            view.findViewById(R.id.command_editor_save).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ci.setCommandName(name.getText().toString());
+                    ci.setCommand(script.getText().toString());
+                    mAdapter.notifyDataSetChanged();
+                }
+            });
+            view.findViewById(R.id.command_editor_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getActivity().getSupportFragmentManager().popBackStack();
+                }
+            });
+            return view;
+        }
     }
 }
